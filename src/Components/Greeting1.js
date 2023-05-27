f180d69ecddb615dfa3e2c3203de6435ce3ce13d
Mohamed Eid <m.mohamedeid11@gmail.com>
2023-05-28 01:38:01 +0300

    compononets

---
+++ src/Components/Greeting1.js
@@ -0,0 +1,14 @@
+import {Component} from "react";
+
+
+class Greeting extends Component {
+    render (){
+        return(
+            <h1>Welcome My App  </h1>
+        )
+    }
+}
+
+export default Greeting;
+
+
---
+++ src/Components/Greeting2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Test(){
+    return (
+        <h1> Mohamed Test</h1>
+    )
+}
+
+export default Test;
+
+
